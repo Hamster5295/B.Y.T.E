9dbc2c32e29bfb134d3abaa58bcf9fb876f5f288
Hamster5295 <hamster5295@163.com>
2019-04-04 22:29:57 +0800

    Add code

--- Assets/Resource/Script/GUI/ByteBtn.cs
+++ Assets/Resource/Script/GUI/ByteBtn.cs
@@ -1,7 +1,26 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace DefaultNamespace
 {
-    public class ByteBtn : UnityEngine.MonoBehaviour
+    public class ByteBtn : MonoBehaviour
     {
+        public Image img;
+        public TextMeshProUGUI text;
         
+        private readonly Color alpha = new Color(0,0,0,0);
+
+        public void OnMouseEnter()
+        {
+            img.color = Color.white;
+            text.color = Color.black;
+        }
+
+        public void OnMouseExit()
+        {
+            img.color = alpha;
+            text.color = Color.white;
+        }
     }
 }
