ec0b70677138191288dfdaa4fb205e8082dfa33d
Hamster5295 <hamster5295@163.com>
2019-04-05 22:16:36 +0800

    Menu

--- Assets/Resource/Script/GUI/ByteBtn.cs
+++ Assets/Resource/Script/GUI/ByteBtn.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace DefaultNamespace
@@ -8,18 +9,37 @@ namespace DefaultNamespace
     {
         public Image img;
         public TextMeshProUGUI text;
+        public AudioSource source;
+        public AudioClip clip;
         
-        private readonly Color alpha = new Color(0,0,0,0);
+        private readonly Color _alpha = new Color(0,0,0,0);
+        private bool _hasAudioClip;
+
+        private void Start()
+        {
+            if (clip == null)
+            {
+                _hasAudioClip = false;
+            }
+            else
+            {
+                source.clip = clip;
+                _hasAudioClip = true;
+            }
+        }
 
         public void OnMouseEnter()
         {
             img.color = Color.white;
             text.color = Color.black;
+            if(_hasAudioClip)
+                source.Play();
+            
         }
 
         public void OnMouseExit()
         {
-            img.color = alpha;
+            img.color = _alpha;
             text.color = Color.white;
         }
     }
